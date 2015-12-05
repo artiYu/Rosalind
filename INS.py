14e78b560aabf96d4a068379fb767324714a67df
lapl4rt <lapl4rt@gmail.com>
2015-12-05 20:05:59 +0300

    Insertion Sort

---
+++ INS.py
@@ -0,0 +1,19 @@
+"""Computing the number of swaps in insertion sort"""
+
+import os
+
+with open(os.getcwd() + "/Data/rosalind_ins.txt", "r") as fr:
+	n = int(fr.readline())
+	a = map(int, fr.readline().strip().split())
+
+def insertionSort(a, c=0):
+	for i in range(1, int(n)):
+		k = i
+		while k >= 1 and a[k] < a[k-1]:
+			a[k], a[k-1] = a[k-1], a[k]
+			c += 1
+			k -= 1
+	return c
+
+with open(os.getcwd() + "/Data/test.txt", "w") as fw:
+	fw.write(str(insertionSort(a, c = 0)))
